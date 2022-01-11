a43881a8cd076350ef5e8557f3090d9c8bad830b
Prakhar Dubey <prakhardubey1321@gmail.com>
2022-01-11 22:39:53 +0530

    Header complete, working on home, creating products

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css';
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 function Header() {
     return (
       <div className="header">
@@ -30,6 +31,12 @@ function Header() {
           <div className="header__options">
             <span className="header__optionLineOne">Your</span>
             <span className="header__optionLineTwo">Prime</span>
+          </div>
+
+          <div className="header__optionBasket">
+              <ShoppingBasketIcon />
+                  <span className='header__optionLineTwo header__basketCount'>0</span>
+              
           </div>
         </div>
       </div>
