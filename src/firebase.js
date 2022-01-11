8d52a0aee1f8f5898f5f636c0c4b4a83b1aac1f8
Prakhar Dubey <prakhardubey1321@gmail.com>
2022-01-11 14:53:19 +0530

    Initial Setup done

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBqd5GxBlNpS89hKO7wyCBA3h__bLaDZn4",
+  authDomain: "clone-c43b0.firebaseapp.com",
+  projectId: "clone-c43b0",
+  storageBucket: "clone-c43b0.appspot.com",
+  messagingSenderId: "288696942798",
+  appId: "1:288696942798:web:e5cb1d7e7b86b0f945109f",
+  measurementId: "G-7F1MTSSRGR",
+};
